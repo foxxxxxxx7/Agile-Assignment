76e22b0ab8c9ab35f95327d6c4ecf944f800b0c2
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-11-22 13:33:35 +0000

    Create write review test.

--- cypress/e2e/reviews.cy.js
+++ cypress/e2e/reviews.cy.js
@@ -21,15 +21,25 @@ describe("The reviews feature", () => {
     describe("Show reviews", () => {
         it("chosen movie has option to see reviews", () => {
           cy.get(".MuiFab-root").click();
-          const rev = reviews.map((r) => r.author);
-          rev.unshift("Author");
+          const allReviews = reviews.map((r) => r.author);
+          allReviews.unshift("Author");
           cy.get("tr").each(($card, index) => {
-            cy.wrap($card).contains(rev[index]);
+            cy.wrap($card).contains(allReviews[index]);
             });
         });
     });
-
-
-
+    
+    describe("Write review", () => {
+        it("favorited movies have option to write review", () => {
+            cy.visit(`/movies`);
+            cy.get("button[aria-label='add to favorites']").eq(1).click();
+            cy.get("button").contains("Favorites").click();
+            cy.get("svg[data-testid='RateReviewIcon']").click();
+            cy.get("input#author").type("Fox");
+            cy.get("textarea#review").type("New Review 12345");
+            cy.get("button[type= 'submit']").click();
+            cy.get("h4.MuiTypography-root").should("be.visible")
+            });
+        });
 
 });
